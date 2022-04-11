5eb08f3c93b0483380e9cd99ec701d85e486ea19
Alex Downey <alexanderdowney@vt.edu>
2022-04-11 17:51:54 -0400

    changed thing

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 # save this as app.py
-from flask import Flask, request
+from flask import Flask, request, render_template
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app,cors_allowed_origins="*")
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return "base site!"
+    # return render_template("index.html")
 
 @app.route('/test', methods=['POST', 'GET'])
 def test():
-    if request.method == 'POST' or request.method == 'GET':
-        language = request.args.get('language')
+    if request.method == 'GET':
+        language = request.args.get('data')
         framework = request.args.get('framework')
+        
+        socketio.emit('update'
+        '''
+        <h1>The language value is: {}</h1>
+        <h1>The framework value is: {}</h1>
+        '''.format(language, framework))
+        
         return '''
                   <h1>The language value is: {}</h1>
                   <h1>The framework value is: {}</h1>'''.format(language, framework)
+    if request.method == 'POST'
+        language = request.form.get('data')
+        framework = request.form.get('framework')
+        return '''
+                  <h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+
+@socketio.on("update")
+def update(data):
+    print('Current Value', data['value'])
 
 
 if __name__ == '__main__':
