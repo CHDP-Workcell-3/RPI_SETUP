cf399f787c84dd47b44cb3748e24d4e52525d274
Alex Downey <alexanderdowney@vt.edu>
2022-04-29 00:27:40 -0400

    flask app up and mostly runnning

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
+import json
+
 # save this as app.py
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__, static_folder="", template_folder="./")
 socketio = SocketIO(app,cors_allowed_origins="*")
+
+data_list = ['temp', 'humidity', 'imu', 'gps']
+collected_data = {data_elem:[] for data_elem in data_list}
 
 @app.route("/")
 def index():
@@ -12,7 +17,6 @@ def index():
 @app.route('/test', methods=['GET'])
 def test():
     
-    data_list = ['temp', 'humidity', 'imu', 'gps']
     data_res = []
     
     if request.method == 'GET':
@@ -21,26 +25,22 @@ def test():
             data_res.append(request.args.get(data))
         
         output = ''
-        
         for data_name, data in zip(data_list, data_res):
             if data != None:
+                collected_data[data_name].append(data)
                 output += '<h1>The {:s} value is: {}</h1>\n'.format(data_name, data)
         
-        socketio.emit('update', output, broadcast=True)
+        socketio.emit('update', json.dumps(collected_data), broadcast=True)
         
         #return output
         return "response sent!"
 
-@socketio.on('hello')
+@socketio.on('connect')
 def handle_hello(data):
-    print(data)
-    socketio.emit('write_back', broadcast=True)
-    #return render_template('index2.html')
+    socketio.emit('update', json.dumps(collected_data), broadcast=True)
 
 
 if __name__ == '__main__':
-    # socketio.run(app, host= '128.173.76.190', port=9000, debug=True)
-    # socketio.run(app, host= '127.0.0.1', port=9000, debug=True)
-    # socketio.run(app, host= '172.20.10.4', port=9000, debug=True)
-    socketio.run(app, host= '10.0.1.4', port=9000, debug=True)
+    # socketio.run(app, host= '10.0.1.4', port=9000, debug=True)
+    socketio.run(app, host= 'localhost', port=9000, debug=True)
     
